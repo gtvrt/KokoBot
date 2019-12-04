fe08b092b5169e4d00c5538b48657f0279fd86ee
gtvrt <56785230+gtvrt@users.noreply.github.com>
2019-12-04 20:27:22 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { getMember, formatDate } = require("./functions.js");
 const { stripIndents } = require("common-tags");
 const fs = require('fs');   
 const { promptMessage } = require("./functions.js");
-const token = "";
+const token = "NjUxNzAzNzE1NzE1MDg4NDA1.XeeldA.mKZSL3DoRgMNinxSuccEyOoJQsU";
 
 
 client.on('ready', () => {
